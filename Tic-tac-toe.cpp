d3feb5f73b250520c4a587583a860852dc716abf
eromero09 <00007224@uca.edu.sv>
2024-07-07 21:07:41 -0600

    LLama a todas las funciones fundamentales y hace un refresco de pantalla

--- Tic-tac-toe.cpp
+++ Tic-tac-toe.cpp
@@ -4,3 +4,12 @@ using namespace std;
 void loop (char c [3][3]);
 void intro_primera (char c [3][3]);
 void tabla (char c [3][3]);
+int main (){
+
+char c [3][3];
+
+loop (c); // LLama a todas las funciones fundamentales y hace un refresco de pantalla.
+
+    system ("pause");
+    return 0;
+}
