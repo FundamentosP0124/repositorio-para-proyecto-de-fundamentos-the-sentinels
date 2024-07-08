7dcce747f14d988427bd1abe63803c59670dd8c7
eromero09 <00007224@uca.edu.sv>
2024-07-07 21:40:39 -0600

    Creacion de columnas

--- Tic-tac-toe.cpp
+++ Tic-tac-toe.cpp
@@ -37,6 +37,10 @@ void tabla (char c [3][3]){
     int i, j;
 
         for (i=0; i < 3; i ++){
+            for (j =0; j < 3; j++){
+            if (j < 2){
+                cout << ( "%c| ", c [i][j]);
+            }   
         }
         cout << ("\n\n");
 }  
